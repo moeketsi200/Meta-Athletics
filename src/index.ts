36ab622768a2df203a05f87878c596107edd1b1a
momotscc-g-025 <mahxmotloung438@gmail.com>
2026-08-19 14:50:05 +0200

    feat: Implement Thrower and Jumper state machine logic

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import {} from '@dcl/sdk/math'
 import { engine } from '@dcl/sdk/ecs'
 
 import { changeColorSystem, circularSystem } from './systems'
+import { throwerStateMachineSystem, jumperStateMachineSystem } from './systems/stateMachines'
 import { setupUi } from './ui'
 
 export function main() {
   // Defining behavior. See `src/systems.ts` file.
   engine.addSystem(circularSystem)
   engine.addSystem(changeColorSystem)
+  engine.addSystem(throwerStateMachineSystem)
+  engine.addSystem(jumperStateMachineSystem)
 
   // draw UI. Here is the logic to spawn cubes.
   setupUi()
